3e9334afc7667abdbbfdeddc5ca1fb150fa8112d
Abdul Khaliq Bin Osman <khaliqand_92@hotmail.com>
2024-03-17 17:04:10 +0800

    Switch imgui to docking

--- src/engine/engine/engine.cpp
+++ src/engine/engine/engine.cpp
@@ -49,6 +49,15 @@ namespace uniengine
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+
+        ImGuiStyle& style = ImGui::GetStyle();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            style.WindowRounding = 0.0f;
+            style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
 
         ImGui_ImplWin32_Init(hWnd);
         ImGui_ImplDX11_Init(device, deviceContext);
@@ -121,6 +130,15 @@ namespace uniengine
 
         ImGui::Render();
         ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+        ImGuiIO& io = ImGui::GetIO();
+        // Update and Render additional Platform Windows
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+        }
+
         m_RenderManager.PostUpdate();
     }
 
